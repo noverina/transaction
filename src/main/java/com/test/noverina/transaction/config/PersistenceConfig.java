245e96a1b78f22f62f19fb5ba0ce255f54047734
Rin <hellenanoverinasari@gmail.com>
2025-08-21 12:44:41 +0700

    fixed hikari init error

--- src/main/java/com/test/noverina/transaction/config/PersistenceConfig.java
+++ src/main/java/com/test/noverina/transaction/config/PersistenceConfig.java
@@ -2,11 +2,16 @@ package com.test.noverina.transaction.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import javax.sql.DataSource;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 @Configuration
@@ -17,5 +22,20 @@ public class PersistenceConfig {
         return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
                 .filter(Authentication::isAuthenticated)
                 .map(Authentication::getName);
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(
+                new ByteArrayResource(
+                        "PRAGMA foreign_keys = ON".getBytes(StandardCharsets.UTF_8)
+                )
+        );
+
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(populator);
+        return initializer;
     }
 }
